45da7829e6815bfc8bddc6e4609e11389c2b02e1
Lyndz456 <lydiah.wangaru@strathmore.edu>
2024-07-02 20:59:30 +0300

    Modified and succesfully managed to be able to review data

--- view_messages.php
+++ view_messages.php
@@ -1,4 +1,5 @@
 <?php include_once ("templates/nav.php")?>
+<?php require_once("includes/dp_connect.php") ?>
 
 <br><br>
 <table  border="1px " cellpadding="5px" style="border: 1px solid ; width: 50%; border-collapse: collapse;">
@@ -8,20 +9,22 @@
                 <th>Fullname</th>
                 <th>email</td>
                 <th>password</th>
+                <th>action</th>
             </tr>
         </thead>
     <tbody>
 <?php
 
 
-$selec_msg = "SELECT * FROM `users` ORDERED BY userid DESC";
+$selec_msg = "SELECT * FROM `users`";
 $selec_msg_result = $conn->query($selec_msg);
+$en =0;
 
-$em =0;
 if ($selec_msg_result->num_rows > 0) {
    
   // output data of each row
   while($selec_msg_row = $selec_msg_result->fetch_assoc()) {
+    
     $en++;
 
     ?>
@@ -32,6 +35,7 @@ if ($selec_msg_result->num_rows > 0) {
         <td> <?php print  $selec_msg_row["email"]; ?> </td> <!--To make the message show just a preview of your message you add
         .' ' .substr($select_msg_row) ["text_message"], 0 ,12) . '...' ;-->
         <td> <?php print  $selec_msg_row["password"]; ?> </td>
+        <td> [<a href="edit_msgs.php">Edit</a> ] [Del] <?php</td>
     
     </tr>
 
